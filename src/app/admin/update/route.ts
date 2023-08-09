3c6783bffb782f251b19e8e7feefdd39eb83442d
leon-do <leondo123@gmail.com>
2023-08-08 23:01:12 -0400

    login page css

--- src/app/admin/update/route.ts
+++ src/app/admin/update/route.ts
@@ -1,5 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
+const { createClient } = require("@supabase/supabase-js");
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json({ message: "Hello from the API!" });
+  // https://supabase.com/docs/reference/javascript/admin-api
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.SUPABASE_SERVICE_ROLE as string, {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+    },
+  });
+
+  // https://github.com/supabase/gotrue-js/issues/359#issuecomment-1203893970
+  const user = await supabase.from("users").select().eq("email", "vietnow1591@gmail.com");
+  if (user.error) return NextResponse.json({ error: "Could Not Find User With Email" }, { status: 400 });
+
+  const id = user.data[0].id;
+  console.log(id);
+
+  // https://supabase.com/docs/reference/javascript/auth-admin-updateuserbyid
+  const status = await supabase.auth.admin.updateUserById(id, {
+    user_metadata: { usage: 1, total: 5 },
+  });
+  if (status.error) return NextResponse.json({ error: "Could Not Update User" }, { status: 400 });
+
+  return NextResponse.json({ success: true });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,5 +12,16 @@ export default function Home() {
     setIsClient(true);
   }, []);
 
-  return <h1>{isClient ? <Auth supabaseClient={supabase} view="magic_link" appearance={{ theme: ThemeSupa }} theme="dark" showLinks={false} providers={[]} redirectTo={`${window.location.href}auth/callback`} /> : ""}</h1>;
+  return (
+    <>
+      <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-sm space-y-10">
+          <div>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login or Sign Up</h2>
+          </div>
+          {isClient ? <Auth supabaseClient={supabase} view="magic_link" appearance={{ theme: ThemeSupa }} theme="light" showLinks={false} providers={[]} redirectTo={`${window.location.href}auth/callback`} /> : ""}
+        </div>
+      </div>
+    </>
+  );
 }
